ce04a3d4644485c9faf087032dd653a008896e7b
jackyhuang <jacky@kequandian.net>
2017-10-16 18:49:34 +0800

    miner

--- sb-log/src/test/java/com/jfeat/am/module/log/api/ut/LogTest.java
+++ sb-log/src/test/java/com/jfeat/am/module/log/api/ut/LogTest.java
@@ -1,15 +1,14 @@
 package com.jfeat.am.module.log.api.ut;
 
-import com.jfeat.base.BaseJunit;
+import com.jfeat.am.module.log.services.crud.persistence.model.OperationLog;
+import com.jfeat.am.module.log.services.crud.service.OperationLogService;
+import com.jfeat.am.BaseJunit;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,10 +17,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 public class LogTest extends BaseJunit {
 
+    @Autowired
+    OperationLogService operationLogService;
+
+    @Before
+    public void initData() {
+        OperationLog log = new OperationLog();
+        log.setLogName("LOGNAME1");
+        log.setLogType("TYPE1");
+        log.setMessage("LOGMESSAGE");
+        operationLogService.createMaster(log);
+    }
+
     @Test
     public void testGetLog() throws Exception {
-        RequestBuilder request = get("/api/logs");
+        RequestBuilder request = get("/api/logs")
+                .requestAttr("userId", "1345");
         MvcResult result = mockMvc.perform(request).andExpect(status().isOk()).andReturn();
-        System.out.println(result.getResponse().getContentAsString());
+        logger.debug(result.getResponse().getContentAsString());
     }
 }
