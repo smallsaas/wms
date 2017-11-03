35505b3e633534586b7dfb7a72864a0b058ec7c0
Silent-Y <1206435482@qq.com>
2017-11-03 11:21:17 +0800

    add sb-notice module

---
+++ sb-organization/src/main/java/com/jfeat/am/module/organization/services/crud/service/impl/DepartmentChildServiceImpl.java
@@ -0,0 +1,42 @@
+package com.jfeat.am.module.organization.services.crud.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jfeat.am.common.crud.impl.CRUDServiceChildImpl;
+import com.jfeat.am.module.organization.services.crud.service.DepartmentChildService;
+import com.jfeat.am.module.organization.services.crud.service.DepartmentService;
+import com.jfeat.am.module.organization.services.persistence.dao.DepartmentMapper;
+import com.jfeat.am.module.organization.services.persistence.dao.StaffMapper;
+import com.jfeat.am.module.organization.services.persistence.model.Department;
+import com.jfeat.am.module.organization.services.persistence.model.Staff;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by Silent-Y on 2017/11/3.
+ */
+public class DepartmentChildServiceImpl extends CRUDServiceChildImpl<Staff, Department> implements DepartmentChildService{
+
+    @Resource
+    private StaffMapper staffMapper;
+    @Resource
+    private DepartmentMapper departmentMapper;
+
+    private static final String childReferenceName = Staff.DEPT_ID;
+
+    @Override
+    protected BaseMapper<Staff> getMasterMapper() {
+        return staffMapper;
+    }
+
+    @Override
+    protected BaseMapper<Department> getChildMapper() {
+        return departmentMapper;
+    }
+
+    @Override
+    protected String getChildReference() {
+        return childReferenceName;
+    }
+}
