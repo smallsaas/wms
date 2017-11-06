bd39bc3970be380f03ccc092298b16ef29da279e
Silent-Y <1206435482@qq.com>
2017-11-06 11:20:27 +0800

    delete package

--- sb-statement/src/main/java/com/jfeat/am/module/statement/services/domain/model/StatementModel.java
+++
@@ -1,8 +0,0 @@
-package com.jfeat.am.module.statement.services.domain.model;
-import com.jfeat.am.module.statement.services.persistence.model.Statement;
-/**
- * Created by Code Generator on 2017-11-06
- */
-public class StatementModel extends Statement{
-
-}
