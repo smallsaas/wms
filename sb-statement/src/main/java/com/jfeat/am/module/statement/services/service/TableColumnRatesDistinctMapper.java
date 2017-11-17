60b1f625d2e60bf5d8b8456e829b48854522d27e
vincent@pc <vincent@jfeat.com>
2017-11-17 18:50:08 +0800

    sb-notice update cnt.

---
+++ sb-statement/src/main/java/com/jfeat/am/module/statement/services/service/TableColumnRatesDistinctMapper.java
@@ -0,0 +1,16 @@
+package com.jfeat.am.module.statement.services.service;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jfeat.am.module.statement.services.model.Equipment;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.Map;
+
+/**
+ * Created by Silent-Y on 2017/11/6.
+ */
+public interface TableColumnRatesDistinctMapper extends BaseMapper<Equipment>{
+    @Select("select #{column} from #{table}")
+    Map queryTableColumnItems(@Param("table") String table, @Param("column") String column);
+}
