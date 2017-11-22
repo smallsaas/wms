7192292106b9dc34eaf572d3b6ff18eda5e00c98
jackyhuang <jacky@kequandian.net>
2017-11-22 09:45:58 +0800

    fix query error

--- sb-notice/src/main/java/com/jfeat/am/module/notice/services/domain/dao/QueryNoticeDao.java
+++ sb-notice/src/main/java/com/jfeat/am/module/notice/services/domain/dao/QueryNoticeDao.java
@@ -3,6 +3,7 @@ package com.jfeat.am.module.notice.services.domain.dao;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.jfeat.am.module.notice.services.persistence.model.Notice;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ import java.util.List;
  * Created by Code Generator on 2017-11-02
  */
 public interface QueryNoticeDao extends BaseMapper<Notice> {
-    List<Notice> findNotices(Page<Notice> page, Notice notice, Integer expired);
+    List<Notice> findNotices(Page<Notice> page, @Param("notice") Notice notice, @Param("expired") Integer expired);
 }
