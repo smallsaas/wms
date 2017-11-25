145c1d55473cee6d87ba62bcf85e9b5676c8f40b
Silent-Y <1206435482@qq.com>
2017-11-25 10:02:54 +0800

    fix the method to get pie data

---
+++ sb-statement/src/main/java/com/jfeat/am/module/statement/services/dao/TableColumnStatusNumDao.java
@@ -0,0 +1,20 @@
+package com.jfeat.am.module.statement.services.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jfeat.am.module.statement.services.model.Equipment;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/8/9.
+ */
+public interface TableColumnStatusNumDao extends BaseMapper<Equipment> {
+
+    Map<String,Integer> getColumnStatusNum(@Param("table") String table, @Param("column") String column, @Param("columnValue") String columnValue, @Param("timeName") String timeName, @Param("startTime") String startTime, @Param("endTime") String endTime);
+
+
+}
+
+
