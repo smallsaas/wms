7cf41be9aed47e9426b5a40d2793a2612e477379
Silent-Y <1206435482@qq.com>
2017-11-28 11:34:48 +0800

    add feedback module

--- sb-feedback/src/main/resources/sql/permission-data.sql
+++ sb-feedback/src/main/resources/sql/permission-data.sql
@@ -1,14 +1,14 @@
 INSERT INTO `perm_group` (`id`, `name`, `identifier`) VALUES
-('935340980606734344', 'TFeedbackImage模块', 'TFeedbackImage.management');
+('935348998345981956', 'TFeedbackImage模块', 'TFeedbackImage.management');
 
 INSERT INTO `perm` (`id`, `groupid`, `name`, `identifier`) VALUES
-('935340980606734341', '935340980606734344', '查看TFeedbackImage', 'TFeedbackImage.view'),
-('935340980606734342', '935340980606734344', '更新TFeedbackImage', 'TFeedbackImage.update'),
-('935340980606734343', '935340980606734344', '删除TFeedbackImage', 'TFeedbackImage.delete');
+('935348998345981953', '935348998345981956', '查看TFeedbackImage', 'TFeedbackImage.view'),
+('935348998345981954', '935348998345981956', '更新TFeedbackImage', 'TFeedbackImage.update'),
+('935348998345981955', '935348998345981956', '删除TFeedbackImage', 'TFeedbackImage.delete');
 INSERT INTO `perm_group` (`id`, `name`, `identifier`) VALUES
-('935340980606734340', 'TFeedback模块', 'TFeedback.management');
+('935348998329204741', 'TFeedback模块', 'TFeedback.management');
 
 INSERT INTO `perm` (`id`, `groupid`, `name`, `identifier`) VALUES
-('935340980606734337', '935340980606734340', '查看TFeedback', 'TFeedback.view'),
-('935340980606734338', '935340980606734340', '更新TFeedback', 'TFeedback.update'),
-('935340980606734339', '935340980606734340', '删除TFeedback', 'TFeedback.delete');
+('935348998329204738', '935348998329204741', '查看TFeedback', 'TFeedback.view'),
+('935348998329204739', '935348998329204741', '更新TFeedback', 'TFeedback.update'),
+('935348998329204740', '935348998329204741', '删除TFeedback', 'TFeedback.delete');
