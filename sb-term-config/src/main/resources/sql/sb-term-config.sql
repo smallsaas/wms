11ea363e872545d0a1e6d3df96d89c2a31135935
J4cob <asdeit@outlook.com>
2017-12-13 18:01:03 +0800

    sql语句

--- sb-term-config/src/main/resources/sql/sb-term-config.sql
+++ sb-term-config/src/main/resources/sql/sb-term-config.sql
@@ -1,15 +1,11 @@
-CREATE TABLE IF NOT EXISTS t_feedback (
-    id BIGINT(20) primary key,
-    user_id BIGINT(20)not null,
-    deal_user_id BIGINT(20)not null,
-    deal_opinion text,
-    content text,
-    created_date timestamp null,
-    unread integer not null default 1
-) ENGINE=InnoDB  DEFAULT CHARSET=UTF8;
+DROP TABLE
+IF EXISTS `t_term_config`;
 
-CREATE TABLE IF NOT EXISTS t_feedback_image (
-    id BIGINT(20) primary key,
-    feedback_id BIGINT(20) not null,
-    url varchar(200)
-) ENGINE=InnoDB  DEFAULT CHARSET=UTF8;
+CREATE TABLE `t_term_config` (
+	id BIGINT (20) NOT NULL,
+	type VARCHAR (50) NOT NULL COMMENT '类型 ABOUT_US,NOTICE,CONNECT_US ...',
+	title VARCHAR (50) NOT NULL COMMENT '标题',
+	content text COMMENT '内容',
+	created_time TIMESTAMP NULL COMMENT '创建时间',
+	PRIMARY KEY (`id`)
+) ENGINE = INNODB DEFAULT CHARSET = UTF8;
