00a162a94efbe3ce8dd6a54bdeb799f6ba6517ad
Silent-Y <1206435482@qq.com>
2018-01-19 18:50:57 +0800

    fix some api

--- sb-organization/src/main/java/com/jfeat/am/module/organization/services/crud/service/impl/DepartmentStaffServiceImpl.java
+++ sb-organization/src/main/java/com/jfeat/am/module/organization/services/crud/service/impl/DepartmentStaffServiceImpl.java
@@ -1,11 +1,14 @@
 package com.jfeat.am.module.organization.services.crud.service.impl;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.jfeat.am.module.organization.constant.IsManager;
 import com.jfeat.am.module.organization.services.crud.service.DepartmentStaffService;
 import com.jfeat.am.module.organization.services.persistence.mapper.DepartmentStaffMapper;
 import com.jfeat.am.module.organization.services.persistence.model.DepartmentStaff;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by Silent-Y on 2018/1/17.
@@ -29,5 +32,20 @@ public class DepartmentStaffServiceImpl implements DepartmentStaffService {
     @Override
     public DepartmentStaff get(DepartmentStaff departmentStaff) {
         return departmentStaffMapper.selectOne(departmentStaff);
+    }
+
+    @Override
+    public Integer update(DepartmentStaff departmentStaff) {
+        return departmentStaffMapper.updateById(departmentStaff);
+    }
+
+    @Override
+    public List<DepartmentStaff> selectList(DepartmentStaff departmentStaff) {
+        return departmentStaffMapper.selectList(new EntityWrapper<DepartmentStaff>().eq(DepartmentStaff.DEPARTMENT_ID,departmentStaff.getDepartmentId()).eq(DepartmentStaff.IS_MANAGER, departmentStaff.getIsManager()));
+    }
+
+    @Override
+    public Integer deleteList(DepartmentStaff departmentStaff) {
+        return departmentStaffMapper.delete(new EntityWrapper<DepartmentStaff>().eq(DepartmentStaff.DEPARTMENT_ID,departmentStaff.getDepartmentId()).eq(DepartmentStaff.IS_MANAGER,departmentStaff.getIsManager()));
     }
 }
