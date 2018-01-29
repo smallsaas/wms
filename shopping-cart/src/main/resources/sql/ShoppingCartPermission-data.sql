643b9c83d96c7f6a1b86abb5504f7ba26e23aa14
J4cob <asdeit@outlook.com>
2018-01-29 09:34:33 +0800

    fixed  sql syntax

--- shopping-cart/src/main/resources/sql/ShoppingCartPermission-data.sql
+++ shopping-cart/src/main/resources/sql/ShoppingCartPermission-data.sql
@@ -1,6 +1,8 @@
+SET FOREIGN_KEY_CHECKS=0;
+
 INSERT INTO `perm_group` (`id`, `name`, `identifier`) VALUES
 ('957169355110281219', 'ShoppingCart模块', 'ShoppingCart.management');
 
 INSERT INTO `perm` (`id`, `groupid`, `name`, `identifier`) VALUES
 ('957169354976063489', '957169355110281219', '查看ShoppingCart', 'ShoppingCart.view'),
-('957169355110281217', '957169355110281219', '更新ShoppingCart', 'ShoppingCart.update'),
+('957169355110281217', '957169355110281219', '更新ShoppingCart', 'ShoppingCart.update');
