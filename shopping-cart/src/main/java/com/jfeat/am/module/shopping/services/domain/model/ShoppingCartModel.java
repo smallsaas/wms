82e6cb31d67f44785933deafed369effeb257b27
J4cob <asdeit@outlook.com>
2018-02-01 16:19:20 +0800

    modified ShoppingCart

--- shopping-cart/src/main/java/com/jfeat/am/module/shopping/services/domain/model/ShoppingCartModel.java
+++ shopping-cart/src/main/java/com/jfeat/am/module/shopping/services/domain/model/ShoppingCartModel.java
@@ -3,6 +3,23 @@ import com.jfeat.am.module.shopping.services.persistence.model.ShoppingCart;
 /**
  * Created by Code Generator on 2018-01-27
  */
-public class ShoppingCartModel extends ShoppingCart{
+public class ShoppingCartModel{
+    Long proId;
+    Integer quantity;
 
+    public Long getProId() {
+        return proId;
+    }
+
+    public void setProId(Long proId) {
+        this.proId = proId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
