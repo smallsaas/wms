5aed053ff10a2d48ff76089a544cd662d4672d4d
Silent-Y <1206435482@qq.com>
2018-02-22 17:52:27 +0800

    add the sb-system-config to save data that can't be change

--- sb-system-config/src/main/resources/sql/system-config.sql
+++ sb-system-config/src/main/resources/sql/system-config.sql
@@ -7,7 +7,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `sb_system_config`;
 CREATE TABLE `sb_system_config` (
   `id` bigint(20) NOT NULL,
-  `data_key` varchar(26) NOT NULL COMMENT 'key',
-  `data_value` varchar(5) NOT NULL COMMENT 'value',
+  `data_key` text NOT NULL COMMENT 'key',
+  `data_value` text NOT NULL COMMENT 'value',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
