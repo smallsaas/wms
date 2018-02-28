e52ae913f60c879727d7d294b643da1fc0c163dc
Silent-Y <1206435482@qq.com>
2018-02-28 18:19:53 +0800

    add the service for ticket attachment item

--- sb-form-attachment/src/main/resources/sql/form-attachment-schema.sql
+++ sb-form-attachment/src/main/resources/sql/form-attachment-schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `ticket_attachment_item` ( /*表单附件明细表*/
  `id` BIGINT(20) NOT NULL,
  `ticket_id` BIGINT(20) NOT NULL COMMENT '表单ID',
  `version` int(11) DEFAULT NULL COMMENT '文档属性版本号',
- `title` VARCHAR(50) DEFAULT NULL COMMENT '文件名',
+ `title` VARCHAR(255) DEFAULT NULL COMMENT '文件名',
  `size` int(11) DEFAULT NULL COMMENT '大小',
  `format` VARCHAR(50) DEFAULT NULL COMMENT '格式[文件后缀]',
  `upload_time` datetime DEFAULT NULL COMMENT '上传时间',
