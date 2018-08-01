35e7c278df744400ba9a26d913734c0068ea7b65
Jacob <commitBy@leuo.com>
2018-08-01 23:44:30 +0800

    delete init data in sku project

--- sku/src/main/resources/sql/sku-data.sql
+++ sku/src/main/resources/sql/sku-data.sql
@@ -1,10 +1,2 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
-
-
-INSERT INTO `t_specification_group` VALUES ('1', '容量', null, 'Category');
-INSERT INTO `t_specification_group` VALUES ('2', '颜色', null, 'Category');
-INSERT INTO `t_specification_group` VALUES ('3', '2G', '1', 'Spec');
-INSERT INTO `t_specification_group` VALUES ('4', '3G', '1', 'Spec');
-INSERT INTO `t_specification_group` VALUES ('5', '白色', '2', 'Spec');
-INSERT INTO `t_specification_group` VALUES ('6', '黑色', '2', 'Spec');
