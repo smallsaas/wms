f74c4e4393e5759e854ca74deb559a9b0e5032ec
root@she11 <she11@kali.com>
2018-08-13 02:49:56 -0400

    delete default data

--- warehouse/src/main/resources/sql/warehouse-data.sql
+++ warehouse/src/main/resources/sql/warehouse-data.sql
@@ -1,13 +1,13 @@
-SET FOREIGN_KEY_CHECKS = 0;
-
-
---for test
-INSERT INTO `wms_warehouse` VALUES ('1', 'WH2018100800059xUV', '第一号仓库', '[\"天津\",\"天津\",\"和平区\"]', '广州市黄埔区彩频路11号', null);
-INSERT INTO `wms_warehouse` VALUES ('2', 'WH2018100800060vhv', '索芙特', '[\"河北\",\"秦皇岛\",\"北戴河区\"]', '广州市黄埔区彩频路11号', null);
-INSERT INTO `wms_warehouse` VALUES ('3', 'WH2018100800061OVy', '测试仓库-2号', '[\"山西\",\"长治\",\"襄垣县\"]', '广州市黄埔区彩频路11号', null);
-INSERT INTO `wms_warehouse` VALUES ('4', 'WH2018100800062ayW', '测试仓库-1号', '[\"河北\",\"秦皇岛\",\"北戴河区\"]', '广州市黄埔区彩频路11号', null);
-
-
-INSERT INTO `wms_suppliers` VALUES ('1', '8.10-1供应商', 'SUP2018100800048fRZ', '[\"北京\",\"北京\",\"东城区\"]', '广东软件园', null, '测试', null, null, null, null, null, null, null, null, null, 'Normal', null, '2018-08-10 12:00:13', null, null);
-INSERT INTO `wms_suppliers` VALUES ('2', '8.10-2供应商', 'SUP2018100800049WHt', '[\"天津\",\"天津\",\"和平区\"]', '新华镇', null, '测试', null, null, null, null, null, null, null, null, null, 'Normal', null, '2018-08-10 12:00:13', null, null);
-INSERT INTO `wms_suppliers` VALUES ('3', '百度科技', 'SUP2018100800050lZO', '[\"北京\",\"北京\",\"东城区\"]', '环市街道23号', '2323233', '李四', '13600000000', null, null, null, null, null, null, null, null, 'Normal', null, '2018-08-10 12:03:52', null, null);
+--SET FOREIGN_KEY_CHECKS = 0;
+--
+--
+----for test
+--INSERT INTO `wms_warehouse` VALUES ('1', 'WH2018100800059xUV', '第一号仓库', '[\"天津\",\"天津\",\"和平区\"]', '广州市黄埔区彩频路11号', null);
+--INSERT INTO `wms_warehouse` VALUES ('2', 'WH2018100800060vhv', '索芙特', '[\"河北\",\"秦皇岛\",\"北戴河区\"]', '广州市黄埔区彩频路11号', null);
+--INSERT INTO `wms_warehouse` VALUES ('3', 'WH2018100800061OVy', '测试仓库-2号', '[\"山西\",\"长治\",\"襄垣县\"]', '广州市黄埔区彩频路11号', null);
+--INSERT INTO `wms_warehouse` VALUES ('4', 'WH2018100800062ayW', '测试仓库-1号', '[\"河北\",\"秦皇岛\",\"北戴河区\"]', '广州市黄埔区彩频路11号', null);
+--
+--
+--INSERT INTO `wms_suppliers` VALUES ('1', '8.10-1供应商', 'SUP2018100800048fRZ', '[\"北京\",\"北京\",\"东城区\"]', '广东软件园', null, '测试', null, null, null, null, null, null, null, null, null, 'Normal', null, '2018-08-10 12:00:13', null, null);
+--INSERT INTO `wms_suppliers` VALUES ('2', '8.10-2供应商', 'SUP2018100800049WHt', '[\"天津\",\"天津\",\"和平区\"]', '新华镇', null, '测试', null, null, null, null, null, null, null, null, null, 'Normal', null, '2018-08-10 12:00:13', null, null);
+--INSERT INTO `wms_suppliers` VALUES ('3', '百度科技', 'SUP2018100800050lZO', '[\"北京\",\"北京\",\"东城区\"]', '环市街道23号', '2323233', '李四', '13600000000', null, null, null, null, null, null, null, null, 'Normal', null, '2018-08-10 12:03:52', null, null);
