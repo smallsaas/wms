cc595a95e48b2948fda956a10aa05dea8f099eb8
root@she11 <she11@kali.com>
2018-08-20 23:44:29 -0400

    action Put -> Post

--- warehouse/src/main/java/com/jfeat/am/module/warehouse/api/crud/TransferEndpoint.java
+++ warehouse/src/main/java/com/jfeat/am/module/warehouse/api/crud/TransferEndpoint.java
@@ -76,14 +76,14 @@ public class TransferEndpoint extends BaseController {
     }
 
     @BusinessLog(name = "Transfer", value = "update Transfer")
-    @PutMapping("/{id}/done")
+    @PostMapping("/{id}/done")
     @ApiOperation(value = "调拨完成",response = TransferModel.class)
     public Tip doneTransfer(@PathVariable Long id) {
         return SuccessTip.create(transferService.doneTransfer(id, JWTKit.getUserId(getHttpServletRequest())));
     }
 
     @BusinessLog(name = "Transfer", value = "update Transfer")
-    @PutMapping("/{id}/cancel")
+    @PostMapping("/{id}/cancel")
     @ApiOperation(value = "调拨作废",response = TransferModel.class)
     public Tip cancelTransfer(@PathVariable Long id) {
         return SuccessTip.create(transferService.cancelTransfer(id,JWTKit.getUserId(getHttpServletRequest())));
