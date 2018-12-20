da0c1ae21c692c2871d1a776ea8e5cb87d21877c
DESKTOP-8TAF8PG\zy <1023839362@qq.com>
2018-12-20 14:17:37 +0800

    message changed

--- sku/src/main/java/com/jfeat/am/module/sku/services/mq/SkuUpdateSender.java
+++ sku/src/main/java/com/jfeat/am/module/sku/services/mq/SkuUpdateSender.java
@@ -2,11 +2,8 @@ package com.jfeat.am.module.sku.services.mq;
 
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +19,13 @@ public class SkuUpdateSender {
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
-    @Autowired
-    private ObjectMapper objectMapper;
+
 
     public void sendUpdateMessage(SkuMessage obj) throws JsonProcessingException {
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         rabbitTemplate.setRoutingKey(SkuMessageConfig.STORE_UPDATE_QUEUE);
-        Message msg= MessageBuilder.withBody(objectMapper.writeValueAsBytes(obj)).build();
         logger.debug("send message = {}", JSONObject.toJSONString(obj));
-        rabbitTemplate.convertAndSend(msg);
+        rabbitTemplate.convertAndSend(JSONObject.toJSONString(obj));
     }
 }
 
