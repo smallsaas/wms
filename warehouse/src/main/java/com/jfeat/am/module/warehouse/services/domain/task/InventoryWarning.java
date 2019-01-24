a1c1bd561100a5dc0fb4979e161b06a9c0a54a2d
J4cob@Windows <asdeit@outlook.com>
2019-01-24 10:18:31 +0800

    预警类

---
+++ warehouse/src/main/java/com/jfeat/am/module/warehouse/services/domain/task/InventoryWarning.java
@@ -0,0 +1,46 @@
+package com.jfeat.am.module.warehouse.services.domain.task;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.jfeat.am.module.sku.services.persistence.dao.SkuProductMapper;
+import com.jfeat.am.module.sku.services.persistence.model.SkuProduct;
+import com.jfeat.am.module.warehouse.services.persistence.dao.InventoryMapper;
+import com.jfeat.am.module.warehouse.services.persistence.model.Inventory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+public class InventoryWarning {
+
+
+
+    @Resource
+    InventoryMapper inventoryMapper;
+    @Resource
+    SkuProductMapper skuProductMapper;
+
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void task(){
+
+        List<Inventory> all = inventoryMapper.selectList(new EntityWrapper<>());
+        if (all == null || all.size()<=0){
+            return;
+        }
+        for (Inventory inventory : all){
+
+            if (inventory.getTransmitQuantities()+inventory.getValidSku() <= inventory.getMinInventory()){
+                SkuProduct skuProduct = skuProductMapper.selectById(inventory.getSkuId());
+
+            }
+            else {
+                return;
+            }
+
+        }
+
+    }
+
+}
