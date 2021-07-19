a60f38c3ca56cb3b771d3e0e967804f6974551f5
huawei laptop <huawei@laptop.com>
2021-07-19 18:58:55 +0800

    upgrade wms to 2.0.0

--- wms-app/src/main/java/com/jfeat/AmApplication.java
+++ wms-app/src/main/java/com/jfeat/AmApplication.java
@@ -1,6 +1,5 @@
 package com.jfeat;
 
-import com.jfeat.crud.core.properties.AmProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +21,13 @@ public class AmApplication extends WebMvcConfigurerAdapter{
 
     protected final static Logger logger = LoggerFactory.getLogger(AmApplication.class);
 
-    @Autowired
-    AmProperties amProperties;
-
     /**
      * 增加swagger的支持
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if(amProperties.getSwaggerOpen()){
-            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-        }
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     public static void main(String[] args) {
